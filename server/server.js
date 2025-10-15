e262f840d53707262f15c381d041546fb743d6c3
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-10-15 21:29:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,37 +1,57 @@
 // Copyright 2025 PREM
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import express from "express"
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from "./config/mongodb.js"
-import imageRouter from "./routes/imageRoutes.js"
-import userRouter from "./routes/userRoutes.js"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import imageRouter from "./routes/imageRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-const port = process.env.PORT || 4000
-const app= express()
+const port = process.env.PORT || 4000;
+const app = express();
 
+app.use(express.json());
+const allowedOrigins = [
+  "https://imagegenerator-client-7izk.onrender.com",
+  "http://localhost:3000",
+  "http://localhost:5173",
+];
 
-app.use(express.json())
-app.use(cors())
-await connectDB()
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or Postman)
+    if (!origin) return callback(null, true);
 
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use('/api/user', userRouter)
-app.use('/api/image', imageRouter)
-app.get('/', (req, res) => {
-    res.send('API Working')
-  });
+app.use(cors(corsOptions));
+await connectDB();
 
-app.listen(port, () => console.log(`Server started on PORT:${port}`))
+app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
